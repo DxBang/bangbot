4e045fa70fafcb7620a962603c83a766d58e393e
Daniel Bang <559371+DxBang@users.noreply.github.com>
2024-02-28 17:38:21 +0100

    Update systems.py

--- extensions/bang/systems.py
+++ extensions/bang/systems.py
@@ -43,7 +43,7 @@ class Systems(commands.Cog, name="Systems"):
 		except Exception as e:
 			await self.bot.error(
 				e,
-				guild = ctx.guild
+				guild = ctx.guild,
 			)
 
 	@commands.command(
@@ -71,7 +71,7 @@ class Systems(commands.Cog, name="Systems"):
 		except Exception as e:
 			await self.bot.error(
 				e,
-				guild = ctx.guild
+				guild = ctx.guild,
 			)
 
 	@commands.hybrid_command(
@@ -136,9 +136,430 @@ class Systems(commands.Cog, name="Systems"):
 		except Exception as e:
 			await self.bot.error(
 				e,
-				guild = ctx.guild
+				guild = ctx.guild,
 			)
 
+	@commands.command(
+		description = "load extension",
+		hidden = True,
+	)
+	@commands.is_owner()
+	async def load(self, ctx:commands.Context, extension:str):
+		try:
+			await self.bot.load_extension("extensions.bang." + extension)
+			await ctx.send(
+				content = f"Loaded: {extension}",
+				reference = ctx.message,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		description = "reload extension",
+		hidden = True,
+	)
+	@commands.is_owner()
+	async def reload(self, ctx:commands.Context, extension:str):
+		try:
+			await self.bot.reload_extension("extensions.bang." + extension)
+			await ctx.send(
+				content = f"Reloaded: {extension}",
+				reference = ctx.message,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		description = "unload extension",
+		hidden = True,
+	)
+	@commands.is_owner()
+	async def unload(self, ctx:commands.Context, extension:str):
+		try:
+			await self.bot.unload_extension("extensions.bang." + extension)
+			await ctx.send(
+				content = f"Unloaded: {extension}",
+				reference = ctx.message,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		aliases = ["activity"],
+		description = "change the custom status of the bot",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def custom(self, ctx:commands.Context, *, text:str):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Custom", value=text)
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = discord.BaseActivity(
+					type = discord.CustomActivity,
+					name = text,
+				)
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		aliases = ["playing"],
+		description = "change the game status of the bot",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def game(self, ctx:commands.Context, *, text:str):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Game", value=text)
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = discord.Game(
+					name = text,
+				)
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		aliases = ["competing"],
+		description = "change the watch status of the bot",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def compete(self, ctx:commands.Context, *, text:str):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Compete", value=text)
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = discord.Activity(
+					type = discord.ActivityType.competing,
+					name = text,
+				)
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		aliases = ["watching"],
+		description = "change the watch status of the bot",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def watch(self, ctx:commands.Context, *, text:str):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Watch", value=text)
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = discord.Activity(
+					type = discord.ActivityType.watching,
+					name = text,
+				)
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		aliases = ["listening"],
+		description = "change the watch status of the bot",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def listen(self, ctx:commands.Context, *, text:str):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Listen", value=text)
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = discord.Activity(
+					type = discord.ActivityType.listening,
+					name = text,
+				)
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		aliases = ["streaming"],
+		description = "change the watch status of the bot",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def stream(self, ctx:commands.Context, *, text:str):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Stream", value=text)
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				#activity = ctx.guild.me.activity
+				activity = discord.Activity(
+					type = discord.ActivityType.streaming,
+					name = text,
+				)
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		aliases = ["hide"],
+		description = "set bot to invisible",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def invisible(self, ctx:commands.Context):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Presence", value="invisible")
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = ctx.guild.me.activity,
+				status = discord.Status.invisible,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		description = "set bot to idle",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def idle(self, ctx:commands.Context):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Presence", value="idle")
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = ctx.guild.me.activity,
+				status = discord.Status.idle,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		description = "set bot to online",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def online(self, ctx:commands.Context):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Presence", value="online")
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = ctx.guild.me.activity,
+				status = discord.Status.online,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		description = "set bot to offline",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def offline(self, ctx:commands.Context):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Presence", value="offline")
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = ctx.guild.me.activity,
+				status = discord.Status.offline,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		description = "set bot to dnd",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def dnd(self, ctx:commands.Context):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Presence", value="dnd")
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = ctx.guild.me.activity,
+				status = discord.Status.dnd,
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
+
+	@commands.command(
+		description = "set bot to afk",
+		hidden = True,
+	)
+	@commands.guild_only()
+	@commands.has_permissions(moderate_members=True)
+	async def afk(self, ctx:commands.Context):
+		try:
+			async with ctx.typing():
+				await asyncio.sleep(1)
+			embed = self.bot.embed(
+				ctx = ctx,
+				bot = True,
+			)
+			embed.add_field(name="Presence", value="afk")
+			await ctx.send(
+				embed = embed,
+				reference = ctx.message,
+			)
+			await self.bot.change_presence(
+				activity = ctx.guild.me.activity,
+				status=ctx.guild.me.status,
+				afk=True
+			)
+		except Exception as e:
+			await self.bot.error(
+				e,
+				guild = ctx.guild,
+			)
 
 
 async def setup(bot:commands.Bot) -> None:
