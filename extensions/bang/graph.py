d5a733c562adc1ab4e811cbfdec3ffefe5eb5906
Daniel Bang <559371+DxBang@users.noreply.github.com>
2024-03-16 17:01:39 +0100

    0.2.28

--- extensions/bang/graph.py
+++ extensions/bang/graph.py
@@ -184,7 +184,7 @@ class Graph(commands.Cog, name="Graph"):
 					Dest.join(temp, jsonFile),
 					data,
 				)
-				wet = "".join(["&#" + str(ord(char)) + ";" for char in "🌧️"])
+				wet = "(wet)"
 				df = pd.DataFrame(data)
 				# create a figure and axis
 				fig = px.line(
@@ -459,7 +459,7 @@ class Graph(commands.Cog, name="Graph"):
 							f"#{cars.get(_car['carId'], {}).get('number', '0')} {ACC.driverName(cars.get(_car['carId']).get('drivers', {}).get(_car['driverIndex'], {}))}",
 							#f"#{cars.get(_car['carId'], {}).get('number', 'N/A')} {cars.get(_car['carId']).get('drivers', {}).get(_car['driverIndex'], {}).get('lastName', 'N/A')}"
 						)
-				wet = "".join(["&#" + str(ord(char)) + ";" for char in "🌧️"])
+				wet = "(wet)"
 				df = pd.DataFrame({
 					"lap": x,
 					"position": y,
